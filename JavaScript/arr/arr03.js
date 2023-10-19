6fd713e4660e6e40eebb47bccaed0988a5d2e8d5
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-10-19 11:05:29 +0900

    Add files via upload

---
+++ JavaScript/arr/arr03.js
@@ -0,0 +1,10 @@
+let scores = [100, 80, 90];
+console.log("국어점수 : "+scores[0]);
+console.log("영어점수 : "+scores[1]);
+console.log("수학점수 : "+scores[2]);
+
+let total = scores[0] + scores[1] + scores[2];
+let average = total/scores.length;
+
+console.log("총점 : " + total);
+console.log("평균 : " + average);
