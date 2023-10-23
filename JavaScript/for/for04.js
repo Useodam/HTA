d752863de5b68012e0e6ee0a87ed87150ee95db5
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-10-23 11:07:56 +0900

    Add files via upload

---
+++ JavaScript/for/for04.js
@@ -0,0 +1,9 @@
+// 구구단 출력하기
+
+for(let dan=2; dan<=3; dan++){
+    console.log(dan+"단");
+    for(let num=1; num<=9; num++){
+        console.log(dan +" * "+ num +" = " + dan*num);
+    }
+    console.log("");
+}
