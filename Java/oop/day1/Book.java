a6daaf2fcf10a6856ffa0eee24413e7aed64eb54
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-11-10 11:36:19 +0900

    책 정보를 표현하는 클래스

---
+++ Java/oop/day1/Book.java
@@ -0,0 +1,16 @@
+package day1;
+
+/**
+ * 책 정보를 표현하는 클래스다
+ */
+public class Book {
+	
+	// 필드, 멤버변수, 프로퍼티 정의하기
+	String title;
+	String author;
+	String publisher;
+	int price;
+	double discountRate;
+	int stock;
+	boolean soldOut;
+}
