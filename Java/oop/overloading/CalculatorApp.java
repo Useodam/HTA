4bf855589bde1360637d35c752b7afd9e650afb0
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-11-13 15:17:19 +0900

    Calculator 클래스 실행

---
+++ Java/oop/overloading/CalculatorApp.java
@@ -0,0 +1,14 @@
+package overloading;
+
+public class CalculatorApp {
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		
+		int result1 = c.sum(10, 20);
+		int result2 = c.sum(10, 20, 40);
+		
+		System.out.println("첫번째 실행결과 : "+result1);
+		System.out.println("두번째 실행결과 : "+result2);
+	}
+}
