23211a5a64cf6bb36244f986898051c7608bda15
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-11-29 14:24:29 +0900

    p3 폴더에 있는 Sample3 클래스의 접근제어자 확인

---
+++ Java/oop/modifier/p4/Sample5.java
@@ -0,0 +1,22 @@
+package modifier.p4;
+
+import modifier.p3.Sample3;
+
+public class Sample5 {
+
+	Sample3 sample3 = new Sample3();
+	
+	public void method() {
+		// private 접근제한이 적용된 value1은 접근할 수 없다
+//		sample3.value1 = 10;	// 컴파일 오류 
+		
+		// 디폴트 접근제한이 적용된 value2는 접근할 수 없다
+//		sample3.value2 = 20;	// 컴파일 오류
+		
+		// protected 접근제한이 적용된 value3는 접근할 수 없다
+//		sample3.value3 = 30;	// 컴파일 오류
+		
+		// public 접근제한이 적용된 value4는 접근할 수 있다 
+		sample3.value4 = 40;
+	}
+}
