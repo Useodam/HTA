a639333e42898142319bf8719d52c58c432e392c
KIM YU SEONG <134251743+Useodam@users.noreply.github.com>
2023-12-07 18:03:24 +0900

    System 클래스의 static 변수

---
+++ Java/api/lang/SystemApp3.java
@@ -0,0 +1,17 @@
+package lang;
+
+public class SystemApp3 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * System 클래스의 static 변수
+		 * 	static final InputStream System.in;
+		 * 		시스템의 표준 입력 스트림 객체를 참조한다
+		 * 	static final PrintStream err
+		 * 		시스템의 표준 에러 출력 스트림 객체를 참조한다
+		 * 	static final PrintStream out
+		 * 		시스템의 표준 출력 스트림 객체를 참조한다
+		 */
+	}
+}
